a566c8d4e6b94b24f50ca927d51424048efaaa52
yymmyymm <109639114+yymmyymm@users.noreply.github.com>
2022-09-01 03:12:32 +0000

    9/1~ middle

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,6 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "482d799460274b63ac89d57ccade6227.vfs.cloud9.ap-northeast-1.amazonaws.com"
   config.active_job.queue_adapter = :inline
+  
+  
 end
